5b2b71cea30e076a4b794e6c7226c32332dbda94
Eric Cornelisesn <ericornelissen@gmail.com>
2019-09-22 12:18:07 +0300

    Output test artifacts to ignored directory

--- tests/example.js
+++ tests/example.js
@@ -1,4 +1,11 @@
+// run using `$ node tests/example.js`
+
+const mkdir = require('make-dir');
+const path = require('path');
 const puppeteer = require('puppeteer');
+
+const ARTIFACTS = path.join(__dirname, '_artifacts');
+mkdir.sync(ARTIFACTS);
 
 (async () => {
   // start browser
@@ -7,27 +14,35 @@ const puppeteer = require('puppeteer');
 
   // Load apge
   await page.goto('http://localhost:4000/');
-  await page.screenshot({path: './tests/1-start.png'});
+  await page.screenshot({path: `${ARTIFACTS}/1-start.png`});
 
   // Go to projects page
   await page.click('header a[href*="projects"]');
-  await page.screenshot({path: './tests/2-projects.png'});
+  await page.screenshot({path: `${ARTIFACTS}/2-projects.png`});
 
   // Go back home
   await page.click('header a[href="/"]');
-  await page.screenshot({path: './tests/3-back-home.png'});
+  await page.screenshot({path: `${ARTIFACTS}/3-back-home.png`});
+
+  // Go to contributions page
+  await page.click('header a[href*="contributions"]');
+  await page.screenshot({path: `${ARTIFACTS}/5-contributions.png`});
+
+  // Go back home
+  await page.click('header a[href="/"]');
+  await page.screenshot({path: `${ARTIFACTS}/6-back-home.png`});
 
   // Go to snippets page
   await page.click('header a[href*="snippets"]');
-  await page.screenshot({path: './tests/4-snippets.png'});
+  await page.screenshot({path: `${ARTIFACTS}/7-snippets.png`});
 
   // Go back home
   await page.click('header a[href="/"]');
-  await page.screenshot({path: './tests/5-back-home.png'});
+  await page.screenshot({path: `${ARTIFACTS}/8-back-home.png`});
 
   // Go to timeline page
   await page.click('header a[href*="timeline"]');
-  await page.screenshot({path: './tests/6-timeline.png'});
+  await page.screenshot({path: `${ARTIFACTS}/9-timeline.png`});
 
   // Close the browser
   await browser.close();
